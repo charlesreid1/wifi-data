3ccc216d3d332f20e715d3fe00d34b4a440dd3db
Charles Reid <charlesreid1@gmail.com>
2016-02-13 10:07:33 -0800

    updating spawnkilldump

--- pi/spawnkilldump.py
+++ pi/spawnkilldump.py
@@ -1,19 +1,20 @@
 import subprocess
 import time
 
-print "\nDon't forget to run this program as root.\n"
+print "\n"
+print "Don't forget to run this program as root.\n"
+print "Don't forget to put your wireless card in monitor mode.\n"
 
-for i in range(3):
+for i in range(10):
 
     print "Beginning round",i+1,"of 10"
 
-    Nmin = 2
+    Nmin = 1
     Nsec = Nmin*60
 
-    the_cmd = ['airodump-ng','wlan1','-w','awesome','--output-format','csv','-M','-u','30']#,'--berlin','%d'%(Nsec),'wlan1']
+    the_cmd = ['airodump-ng','wlan0','-w','awesome','--output-format','csv','-M','-u','30']#,'--berlin','%d'%(Nsec),'wlan1']
 
     print " ".join(the_cmd)
-    #print the_cmd
 
     p = subprocess.Popen(the_cmd)
 
