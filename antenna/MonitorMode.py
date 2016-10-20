0b3001df2a27b5f97eb521e4742b739bab3c85c8
Charles Reid <charlesreid1@gmail.com>
2016-10-19 22:46:12 -0500

    script to put wifi interfaces in monitor mode

--- antenna/MonitorMode.py
+++ antenna/MonitorMode.py
@@ -1,1 +1,8 @@
-#
+import subprocess
+
+wlan_name = "wlan0"
+
+subprocess.call(["ifconfig",wlan_name,"down"])
+subprocess.call(["iwconfig",wlan_name,"mode","monitor"])
+subprocess.call(["ifconfig",wlan_name,"up"])
+
